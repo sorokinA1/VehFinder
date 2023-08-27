d3da226e464b6621204c99d2471a4d4d87bd2ece
artem <email.@mail.com>
2023-08-27 10:11:40 +0500

    first commit

---
+++ VehFinder/Program.cs
@@ -0,0 +1,90 @@
+﻿// var vehFinderApp = new VehFinderApp(new VehiclesRepository(new StringsTextualRepository()));
+// vehFinderApp.Init();
+
+
+
+
+void ClickQuickSearchSimulator()
+{
+    Console.WriteLine("Click");
+}
+
+
+public class VehFinderApp
+{
+    private readonly IVehRepository _vehRepository;
+
+    public VehFinderApp(IVehRepository vehRepository)
+    {
+        _vehRepository = vehRepository;
+    }
+
+    public void Init()
+    {
+       var x = _vehRepository.Read("vehicles.txt");
+
+       foreach (var s in x)
+       {
+           Console.WriteLine(s);
+       }
+    }
+}
+
+public interface IVehRepository
+{
+    List<string> Read(string filePath);
+}
+
+public class VehiclesRepository : IVehRepository
+{
+    
+    private readonly IStringsRepository _stringsRepository;
+
+    
+    public VehiclesRepository(IStringsRepository stringsRepository)
+    {
+        _stringsRepository = stringsRepository;
+    }
+    
+    
+    public List<string> Read(string filePath)
+    {
+        List<string> VehiclesFromFile = _stringsRepository.Read(filePath);
+        var vehicles = new List<string>();
+
+        foreach (var vehicle in VehiclesFromFile)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        return vehicles;
+    }
+}
+
+public interface IStringsRepository
+{
+    List<string> Read(string filePath);
+}
+
+public abstract class StringsRepository : IStringsRepository
+{
+    public List<string> Read(string filePath)
+    {
+        if (!File.Exists(filePath)) throw new FileNotFoundException("File vehicles.txt is not found");
+        var fileContents = File.ReadAllText(filePath);
+        return TextToStrings(fileContents);
+    }
+
+    protected abstract List<string> TextToStrings(string fileContents);
+}
+
+public class StringsTextualRepository : StringsRepository
+{
+    private static readonly string Separator = Environment.NewLine;
+
+    protected override List<string> TextToStrings(string fileContents)
+    {
+        return fileContents.Split(Separator).ToList();
+    }
+}
+
